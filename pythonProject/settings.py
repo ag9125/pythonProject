52fc99beda93dbef901788af0e3f31db1a79aaa2
ag9125 <147823078+ag9125@users.noreply.github.com>
2024-12-26 20:48:30 +0530

    Update settings.py

--- pythonProject/settings.py
+++ pythonProject/settings.py
@@ -84,22 +84,22 @@ WSGI_APPLICATION = 'pythonProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sql5754281',
-        'USER': 'sql5754281',
-        'PASSWORD': 'Zva44gJHgU',
-        'HOST': 'sql5.freesqldatabase.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sql5754281',
+#         'USER': 'sql5754281',
+#         'PASSWORD': 'Zva44gJHgU',
+#         'HOST': 'sql5.freesqldatabase.com',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
